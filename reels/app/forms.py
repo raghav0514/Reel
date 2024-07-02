3c50cb00870c2422a8f5c1b063431f68bfd26532
RRV <143807508+raghava0914@users.noreply.github.com>
2024-07-02 13:14:45 +0530

    Delete reels/app directory

--- reels/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, FileField
-from wtforms.validators import DataRequired
-
-class UploadForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    video = FileField('Video', validators=[DataRequired()])
