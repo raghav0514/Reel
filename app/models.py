733145b8e0e50a91387b534ee220d8db421f23f0
RRV <143807508+raghava0914@users.noreply.github.com>
2024-07-02 13:32:25 +0530

    Delete app directory

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Reel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    filename = db.Column(db.String(100), nullable=False)
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
